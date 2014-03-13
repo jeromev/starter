4305c93ea56792026fda2f84e449ece52932c5b2
Jerome Vogel <jv@jeromevogel.com>
2014-03-13 14:51:32 -0400

    added data-useragent attr

--- starter.js
+++ starter.js
@@ -1,1 +1,6 @@
 document.write('<script src="http://code.jquery.com/jquery-1.11.0.min.js"><\/script>');
+
+// ---> BEGIN add useragent attr (src: http://css-tricks.com/ie-10-specific-styles)
+var doc = document.documentElement;
+doc.setAttribute('data-useragent', navigator.userAgent);
+// <--- END add useragent attr
