78c067a3f06fe4349492e8c3c4b3a70ec2ab5599
Jerome Vogel <jv@jeromevogel.com>
2014-03-15 13:51:04 -0400

    try again

--- starter.js
+++ starter.js
@@ -1,9 +1,4 @@
-document.write(
-  '<script src="http://code.jquery.com/jquery-1.11.0.min.js"><\/script>
-  <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"><\/script>
-  <![endif]-->'
-);
+document.write('<script src="http://code.jquery.com/jquery-1.11.0.min.js"><\/script><!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"><\/script><![endif]-->');
 
 // ---> BEGIN add useragent attr (src: http://css-tricks.com/ie-10-specific-styles)
 var doc = document.documentElement;
